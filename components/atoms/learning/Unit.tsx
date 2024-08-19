69462d4d2d49d605494c5b115fd1715ddadcdaea
yemyatthu-binlab <yemyatthu.cs@gmail.com>
2024-08-19 17:08:39 +0630

    DONE:: display total lesson list and unit header in current active learning course

---
+++ components/atoms/learning/Unit.tsx
@@ -0,0 +1,53 @@
+import { lessons, units } from "@/database/schema";
+
+import { LessonButton } from "./lessonButton";
+import { UnitBanner } from "./unitBanner";
+
+type UnitProps = {
+  id: number;
+  order: number;
+  title: string;
+  description: string;
+  lessons: (typeof lessons.$inferSelect & {
+    completed: boolean;
+  })[];
+  activeLesson:
+    | (typeof lessons.$inferSelect & {
+        unit: typeof units.$inferSelect;
+      })
+    | undefined;
+  activeLessonPercentage: number;
+};
+
+export const Unit = ({
+  title,
+  description,
+  lessons,
+  activeLesson,
+  activeLessonPercentage,
+}: UnitProps) => {
+  return (
+    <>
+      <UnitBanner title={title} description={description} />
+
+      <div className="relative flex flex-col items-center bg-[url('/lesson-path-bg-pattern.svg')] bg-center bg-repeat-y">
+        {lessons.map((lesson, i) => {
+          const isCurrent = lesson.id === activeLesson?.id;
+          const isLocked = !lesson.completed && !isCurrent;
+
+          return (
+            <LessonButton
+              key={lesson.id}
+              id={lesson.id}
+              index={i}
+              totalCount={lessons.length - 1}
+              current={isCurrent}
+              locked={isLocked}
+              percentage={activeLessonPercentage}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+};
