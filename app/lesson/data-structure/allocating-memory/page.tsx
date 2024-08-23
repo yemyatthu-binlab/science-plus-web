00fc92915787b22550d758a9da52cb65cad8b5c5
yemyatthu-binlab <yemyatthu.cs@gmail.com>
2024-08-24 02:16:49 +0630

    WIP:: add lesson 2 intro

---
+++ app/lesson/data-structure/allocating-memory/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { HeaderProgress } from "@/components/atoms/common/headerProgress";
+import ArrayExplanation from "@/components/molecules/lessons/ds_course/ls1/arrayExplanation";
+import DSIntro from "@/components/molecules/lessons/ds_course/ls1/dsIntro";
+import Lesson1Review from "@/components/molecules/lessons/ds_course/ls1/ls1Review";
+import RecordExplanation from "@/components/molecules/lessons/ds_course/ls1/recordExplanation";
+import WhatIsDs from "@/components/molecules/lessons/ds_course/ls1/whatIsDs";
+import AllocatingMemoryIntro from "@/components/molecules/lessons/ds_course/ls2/allocatingMemoryIntro";
+import { useState } from "react";
+
+const DSLesson = () => {
+  const [progress, setProgress] = useState(1);
+  const numberOfSection = 5;
+  return (
+    <div>
+      <HeaderProgress
+        hearts={100}
+        percentage={progress * (100 / numberOfSection)}
+        hasActiveSubscription={false}
+      />
+      <div className="max-w-[560px] mx-auto mt-5">
+        {progress == 1 && (
+          <AllocatingMemoryIntro
+            handleClick={() => setProgress(progress + 1)}
+          />
+        )}
+        {/* {progress == 2 && (
+          <WhatIsDs handleClick={() => setProgress(progress + 1)} />
+        )}
+        {progress == 3 && (
+          <ArrayExplanation handleClick={() => setProgress(progress + 1)} />
+        )}
+        {progress == 4 && (
+          <RecordExplanation handleClick={() => setProgress(progress + 1)} />
+        )}
+        {progress == 5 && <Lesson1Review />} */}
+      </div>
+    </div>
+  );
+};
+
+export default DSLesson;
